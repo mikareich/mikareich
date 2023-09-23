9a24292f551878fe2b122be78f2180611af26e39
Mika Reich <mikareich0@gmail.com>
2023-09-23 16:02:56 +0200

    change footer bg for higher contrast

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="mt-auto">
-      <footer className="mt-[100px] flex h-[100px] w-full items-center justify-center bg-raisin-black-400">
+      <footer className="mt-[100px] flex h-[100px] w-full items-center justify-center bg-raisin-black-200">
         <span className="text-baby-powder">Designed & built by Mika Reich</span>
       </footer>
     </div>
