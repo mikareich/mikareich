3f561cd1bb1a0515cf29bcf37f8449751360145a
Mika Reich <mikareich0@gmail.com>
2023-09-24 12:22:34 +0200

    This is the last time we let Wade commit ascii porn in the comments.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,16 +2,18 @@ import MdxContent from '@/components/MdxContent'
 import { getNotFoundPage } from '@/utils/pageUtils'
 
 export async function generateMetadata() {
-  const { frontMatter } = (await getNotFoundPage())!
+  const p = await getNotFoundPage()
+
+  console.log(p)
 
   return {
-    title: `Mika Reich | ${frontMatter.title}`,
-    description: frontMatter.description,
+    title: `Mika Reich | ${p?.frontMatter.title}`,
+    description: p?.frontMatter.description,
   }
 }
 
 export default async function NotFound() {
-  const { content } = (await getNotFoundPage())!
+  const p = await getNotFoundPage()
 
-  return <MdxContent source={content} />
+  return <MdxContent source={p?.content || ''} />
 }
--- components/ContextProvider.tsx
+++ components/ContextProvider.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { createContext, useMemo, useState } from 'react'
+import { createContext, useState } from 'react'
 
 export const useDrawer = () => {
   const [drawerOpen, setDrawerOpen] = useState(false)
