83cbd2dc46406b2f2597b8d964a9f3adbc40cc68
Mika Reich <mikareich0@gmail.com>
2023-09-24 17:18:37 +0200

    remove meta data

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,6 @@
 import MdxContent from '@/components/MdxContent'
 import { getNotFoundPage } from '@/utils/pageUtils'
 
-export async function generateMetadata() {
-  const { frontMatter } = (await getNotFoundPage())!
-
-  return {
-    title: `Mika Reich | ${frontMatter.title}`,
-    description: frontMatter.description,
-  }
-}
-
 export default async function NotFound() {
   const { content } = (await getNotFoundPage())!
 
