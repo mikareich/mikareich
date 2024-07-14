3df131a8ad918a51dcb62aa14841d71e3c448a74
Mika Reich <mikareich0@gmail.com>
2024-07-15 00:52:13 +0200

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-    mdx(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), react(), mdx()],
   output: "hybrid",
+  adapter: vercel()
 });
